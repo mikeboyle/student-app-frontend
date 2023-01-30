e41b506f39edfd41e508f6675a51911d7ed333df
Mike Boyle <mikesboyle@gmail.com>
2023-01-30 10:47:04 -0500

    changes from group codealong

--- src/components/StudentList/StudentList.js
+++ src/components/StudentList/StudentList.js
@@ -1,14 +1,36 @@
+import { useState } from 'react';
 import StudentCard from '../StudentCard/StudentCard';
 
 import './StudentList.css';
 
 const StudentList = ({ studentData }) => {
+  const [searchInput, setSearchInput] = useState('');
+
+  // create var to store filtered data
+  const filteredData = studentData.filter((student) => {
+    // create new var for the full name
+    const fullName = student.firstName + ' ' + student.lastName;
+
+    return fullName.toLowerCase().includes(searchInput.toLowerCase());
+  });
+
   console.log(`<StudentList /> rendered!`);
   return (
     <div className="StudentList">
-      {studentData.map((student) => (
+      <input
+        value={searchInput}
+        onChange={(e) => {
+          setSearchInput(e.target.value);
+        }}
+        type="text"
+        placeholder="Search by name"
+      />
+      {filteredData.map((student) => (
         <StudentCard key={student.id} student={student} />
       ))}
+      {filteredData.length === 0 && (
+        <div>No students found for {searchInput}</div>
+      )}
     </div>
   );
 };
